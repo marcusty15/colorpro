6f5e7095f7ce08f85aac53cde7dee1fa231b8866
marcusty15 <marco.troconis15@gmail.com>
2023-04-19 18:32:57 -0400

    se ajusto las cards y se centro los botones

--- src/pages/cardpage/Cardpages.js
+++ src/pages/cardpage/Cardpages.js
@@ -30,9 +30,9 @@ return (
         </div>
         <img className="img " src={img} alt=""/>
       <form className=" m-4 buscador text-center w-100 " role="search">
-        <h5 className="h5busc  text-center">Bienvenidos al sistema de rastreo de pedidos por favor ingresa su numero de pedido:</h5>
+        <h5 className="h5busc  text-center">¡Hola! Bienvenido al sistema de rastreo de pedidos, por favor ingresa su número de pedido para conocer su estatus:</h5>
         <div class="input-group mb-3">
-  <input  onChange={e=>setSearch(e.target.value)} type="number" class="form-control border-primary " placeholder="Nombre de Pedido" aria-label="Nombre de usuario del destinatario" aria-describedby="button-addon2"/>
+  <input  onChange={e=>setSearch(e.target.value)} type="number" class="form-control border-primary " placeholder="Numero de pedido" aria-label="Nombre de usuario del destinatario" aria-describedby="button-addon2"/>
   <button onClick={obtenerRecursos} class="btn btn-primary btn-outline-light" type="button" id="button-addon2">Buscar</button>
 </div>
       </form>
@@ -45,7 +45,7 @@ return (
           )}
         </div>
         <div className=" w-100 justify-content-center text-center">
-           <h3 className="h3contact  ">Si tienes alguna duda o no sabes tu numero de pedido, contáctenos haciendo click en el siguiente botón</h3>
+           <h3 className="h3contact  ">Si usted tiene alguna duda o no sabes tu numero de pedido, contáctenos haciendo click en el siguiente botón</h3>
             <a className="text-center" href="https://wa.link/5i79eu"><button type="button" class="btn btn-success  boton whats shadow">WhatsApp</button></a>
         </div>
       </div>
